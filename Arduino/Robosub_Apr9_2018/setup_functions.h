6cc5fa109b19f9e1cb78c869dd536291419f1cf5
Connor Strang <oceanstrang@gmail.com>
2018-07-28 00:33:45 -0400

    Brought setup_functions.h up to date
    Removed TFT init code as that will not be used for the foreseeable future.
    Added preprocessor structure to select which IMU to setup and switched to specific model names for all sensors. The actual #define to select the IMU is in the main file.

--- Arduino/Robosub_Apr9_2018/setup_functions.h
+++ Arduino/Robosub_Apr9_2018/setup_functions.h
@@ -1,47 +1,58 @@
 
 bool IMU_SETUP_GOOD = false;
 void setupIMU() {
+#ifdef IMU_BNO055
   Serial.println("BOOT Initializing IMU...");
   unsigned long init_begin = millis();
   unsigned int timeout = 5000;
   int cnt = 1;
   while(millis() - init_begin < timeout) {
-    if(bno.begin(Adafruit_BNO055::OPERATION_MODE_NDOF)) {
+    if(bno055.begin(Adafruit_BNO055::OPERATION_MODE_NDOF)) {
       IMU_SETUP_GOOD = true;
       break;
     }
-    String msg = "BOOT\tAttempt " + String(cnt) + " failed.";
-    Serial.print(msg);
+    String msg = "BOOT\tIMU Init - Attempt " + String(cnt) + " failed.";
+    Serial.println(msg);
     ++cnt;
     delay(500);
   }
   if(IMU_SETUP_GOOD) {
     Serial.println(F("BOOT\tIMU found."));
+    uint8_t sys, gyro, accel, mag;
+    bno055.getCalibration(&sys, &gyro, &accel, &mag); // this blocks the entire program if it fails to get the data
+    String msg = String("INFO Imu: Sys = ") + sys + ", Gyro = " + gyro + ", Accel = " + accel + ", Mag = " + mag; Serial.println(msg);
   } else {
     Serial.println(F("BOOT\tIMU initialization failed. Continuing... even though it's pointless."));
   }
 
-  bno.setExtCrystalUse(true);
-}
+  bno055.setExtCrystalUse(true);
 
-bool TFT_SETUP_GOOD = true; // can't really verify this...
-void setupTFT() {
-  Serial.println(F("BOOT Initializing TFT display..."));
-  tft.begin();
-  tft.fillScreen(ILI9341_WHITE);
-  tft.setTextColor(ILI9341_BLACK);
-  tft.setTextSize(2);
+#elif defined(IMU_MPU6050)
+  Serial.println("BOOT Initializing IMU...");
+  Wire.begin();
+  mpu6050.begin();
+  Serial.println("BOOT IMU initialized if connected correctly.");
+  Serial.println("BOOT Beginning IMU Calibration - DO NOT MOVE");
+  mpu6050.calcGyroOffsets(false); // do not write to console
+  String msg = "BOOT\tIMU gyro calibration complete: " + String(mpu6050.getGyroXoffset()) + ", "
+    + String(mpu6050.getGyroYoffset()) + ", " + String(mpu6050.getGyroZoffset());
+  Serial.println(msg);
 
-  tft.setRotation(2);
+/*
+cal log:
+-13.29, 1.76, -0.73
+-13.14, 1.77, -0.71
+-13.25, 1.65, -0.69
 
-  Serial.println(F("BOOT\tTFT setup complete (assuming it's attached)"));
+*/
+
+#endif
 }
 
 
 void setupPeripherals() {
   setupIMU();
-  setupTFT();
-  depth_sensor.begin();
+  ms5803.begin();
 }
 
 
